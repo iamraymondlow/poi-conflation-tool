7e5127d27f7b6a5c284624b4339e680cdeb6c5c7
Raymond Low <iamraymondlow@gmail.com>
2021-07-08 15:40:11 +0800

    included function to convert local json cache as geopandas

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
 import os
 import model
+import pandas as pd
+import geopandas as gpd
 from onemap_downloader import OneMap
 from osm_processor import OSM
 from sla_processor import SLA
@@ -21,29 +23,60 @@ class POIConflationTool:
         not formatted, the appropriate functions will be triggered to begin formatting the dataset.
         Also checks if machine learning model for identifying POI duplicates are trained.
         """
-        # check if OneMap data is formatted. If not, format OneMap data.
-        if not os.path.exists(os.path.join(os.path.dirname(__file__), config['onemap_output'])):
+        # load formatted OneMap data. If it does not exist, format and load data.
+        print('Loading OneMap data from local directory...')
+        if not os.path.exists(os.path.join(os.path.dirname(__file__), config['onemap_cache'])):
             OneMap().format_data()
+        self.onemap_data = self._load_json_as_geopandas(config['onemap_cache'])
 
-        # check if SLA data is formatted. If not, format SLA data.
-        if not os.path.exists(os.path.join(os.path.dirname(__file__), config['sla_output'])):
+        # load formatted SLA data. If it does not exist, format and load data.
+        print('Loading SLA data from local directory...')
+        if not os.path.exists(os.path.join(os.path.dirname(__file__), config['sla_cache'])):
             SLA().format_data()
+        self.sla_data = self._load_json_as_geopandas(config['sla_cache'])
 
-        # check if OSM data is formatted. If not, format OSM data.
-        if not os.path.exists(os.path.join(os.path.dirname(__file__), config['osm_output'])):
+        # load formatted OSM data. If it does not exist, format and load data.
+        print('Loading OSM data from local directory...')
+        if not os.path.exists(os.path.join(os.path.dirname(__file__), config['osm_cache'])):
             OSM().format_data()
+        self.osm_data = self._load_json_as_geopandas(config['osm_cache'])
+
+        # load formatted Google data. If it does not exist, save as None.
+        print('Loading Google data from local directory...')
+        if os.path.exists(os.path.join(os.path.dirname(__file__), config['google_cache'])):
+            self.google_data = self._load_json_as_geopandas(config['google_cache'])
+        else:
+            self.google_data = None
+
+        # load formatted HERE Map data. If it does not exist, save as None.
+        print('Loading HERE Map data from local directory...')
+        if os.path.exists(os.path.join(os.path.dirname(__file__), config['here_cache'])):
+            self.here_data = self._load_json_as_geopandas(config['here_cache'])
+        else:
+            self.here_data = None
 
         # check if machine learning model is trained. If not, train model.
-        if not os.listdir(os.path.exists(os.path.join(os.path.dirname(__file__), config['models_directory']))):
-            model.train_model()
-
-        # load local POI data and machine learning models
-        self.osm_data = None
-        self.onemap_data = None
-        self.sla_data = None
-        self.google_data = None
-        self.heremap_data = None
+        # if not os.listdir(os.path.exists(os.path.join(os.path.dirname(__file__), config['models_directory']))):
+        #     model.train_model()
         self.models = None
+
+    def _load_json_as_geopandas(self, cache_directory):
+        """
+        This function loads a local JSON file and converts it into a Geodataframe.
+
+        :param cache_directory: str
+            Contains the directory for the cache POI data file.
+        :return:
+        data: geopandas
+            Contains the cached POI data file formatted as a Geodataframe.
+        """
+        with open(os.path.join(os.path.dirname(__file__), cache_directory)) as file:
+            data_json = json.load(file)
+        data = pd.json_normalize(data_json['features'])
+        data = gpd.GeoDataFrame(data,
+                                geometry=gpd.points_from_xy(data['geometry.lng'],
+                                                            data['geometry.lat']))
+        return data
 
     def extract_poi(self, lat, lng):
         """
@@ -67,6 +100,11 @@ class POIConflationTool:
         return None
 
 
-
-
-
+if __name__ == '__main__':
+    tool = POIConflationTool()
+    osm_data = tool.osm_data
+    onemap_data = tool.onemap_data
+    sla_data = tool.sla_data
+    google_data = tool.google_data
+    heremap_data = tool.here_data
+    models = tool.models
